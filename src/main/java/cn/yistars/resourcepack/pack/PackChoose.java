5705ed0270ed819f2d6763e28a94b602e153fab0
冰砚炽 <Bing_Yanchi@outlook.com>
2023-06-03 03:15:11 +0800

    匹配规则

--- src/main/java/cn/yistars/resourcepack/pack/PackChoose.java
+++ src/main/java/cn/yistars/resourcepack/pack/PackChoose.java
@@ -1,7 +1,37 @@
 package cn.yistars.resourcepack.pack;
 
+import cn.yistars.resourcepack.config.ConfigManager;
+
 public class PackChoose {
     public static ResourcePack getPack(String serverName) {
-        return null;
+
+        // 先判断是否属于指定服务器名称的匹配组
+        for (String key : ConfigManager.config.getSection("match-rules").getKeys()) {
+            if (!PackManager.packs.containsKey(ConfigManager.config.getString("match-rules." + key + ".pack"))) continue;
+
+            if (ConfigManager.config.contains("match-rules." + key + ".servers")) {
+                if (ConfigManager.config.getStringList("match-rules." + key + ".servers").contains(serverName)) {
+                    return PackManager.packs.get(ConfigManager.config.getString("match-rules." + key + ".pack"));
+                }
+            }
+        }
+
+        // 再判断是否符合正则表达式
+        for (String key : ConfigManager.config.getSection("match-rules").getKeys()) {
+            if (!PackManager.packs.containsKey(ConfigManager.config.getString("match-rules." + key + ".pack"))) continue;
+
+            if (ConfigManager.config.contains("match-rules." + key + ".regex")) {
+                if (serverName.matches(ConfigManager.config.getString("match-rules." + key + ".regex"))) {
+                    return PackManager.packs.get(ConfigManager.config.getString("match-rules." + key + ".pack"));
+                }
+            }
+        }
+
+        // 判断默认包是否存在
+        if (!PackManager.packs.containsKey(ConfigManager.config.getString("empty-resource-pack-name"))) {
+            return null;
+        }
+
+        return PackManager.packs.get(ConfigManager.config.getString("empty-resource-pack-name"));
     }
 }
--- src/main/java/cn/yistars/resourcepack/pack/PackManager.java
+++ src/main/java/cn/yistars/resourcepack/pack/PackManager.java
@@ -2,16 +2,16 @@ package cn.yistars.resourcepack.pack;
 
 import cn.yistars.resourcepack.config.ConfigManager;
 
-import java.util.HashSet;
+import java.util.HashMap;
 
 public class PackManager {
-    public static HashSet<ResourcePack> packs = new HashSet<>();
+    public static HashMap<String, ResourcePack> packs = new HashMap<>();
 
     public static void reloadPack() {
         packs.clear();
 
         for (String key : ConfigManager.config.getSection("resource-packs").getKeys()) {
-            packs.add(new ResourcePack(
+            packs.put(key, new ResourcePack(
                     key,
                     ConfigManager.config.getString("resource-packs." + key + ".url"),
                     ConfigManager.config.getBoolean("resource-packs." + key + ".force", false),
